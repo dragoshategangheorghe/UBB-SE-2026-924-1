1fc671978090591aff6f76cc002c2c1b685b0b80
Kovács Levente <le.kovacs789@gmail.com>
2026-05-09 08:38:45 +0300

    created repo proxy for Investments API

---
+++ BankApp/BankApp.Client/RepositoryProxies/Implementations/InvestmentRepositoryProxy.cs
@@ -0,0 +1,32 @@
+﻿using BankApp.Client.RepositoryProxies.Interfaces;
+using BankApp.Client.Utilities;
+using BankApp.Models.DTOs.Profile;
+using BankApp.Models.Features.Investments;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Implementations
+{
+    public class InvestmentRepositoryProxy : IInvestmentRepositoryProxy
+    {
+        private readonly ApiService apiService;
+
+        public InvestmentRepositoryProxy(ApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public Task<Portfolio?> GetPortfolioAsync(int userId)
+        {
+            return this.apiService.GetAsync<Portfolio>($"/api/investments/portfolio/{userId}");
+        }
+
+        public Task<IActionResult> TradeAsync(dynamic tradeData)
+        {
+            // Note: I don't know what request I'm supposed to use here
+            //  I'm just going to use the variable like this, it's fishy anyway
+            //  I also don't know about the response...
+            return this.apiService.PostAsync<dynamic, IActionResult>($"/api/investments/trade", tradeData);
+        }
+    }
+}
---
+++ BankApp/BankApp.Client/RepositoryProxies/Interfaces/IInvestmentRepositoryProxy.cs
@@ -0,0 +1,17 @@
+﻿using BankApp.Models.Features.Investments;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Interfaces
+{
+    public interface IInvestmentRepositoryProxy
+    {
+        Task<Portfolio?> GetPortfolioAsync(int userId);
+
+        Task<IActionResult> TradeAsync(dynamic tradeData);
+    }
+}
