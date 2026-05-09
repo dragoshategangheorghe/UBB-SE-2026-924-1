ef2d35bff2d1c90d3942cd95a66533665fbb2e49
Kovács Levente <le.kovacs789@gmail.com>
2026-05-09 09:31:03 +0300

    created repo proxy for LoanPresentationAPI

---
+++ BankApp/BankApp.Client/RepositoryProxies/Implementations/LoanPresentationRepositoryProxy.cs
@@ -0,0 +1,26 @@
+﻿using BankApp.Client.RepositoryProxies.Interfaces;
+using BankApp.Client.Utilities;
+using BankApp.Models.Features.Loans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Implementations
+{
+    public class LoanPresentationRepositoryProxy : ILoanPresentationRepositoryProxy
+    {
+        private readonly ApiService apiService;
+
+        public LoanPresentationRepositoryProxy(ApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public Task<double> GetRepaymentProgressAsync(Loan loan)
+        {
+            return this.apiService.PostAsync<Loan, double>("/api/loans/repayment-progress", loan);
+        }
+    }
+}
---
+++ BankApp/BankApp.Client/RepositoryProxies/Interfaces/ILoanPresentationRepositoryProxy.cs
@@ -0,0 +1,14 @@
+﻿using BankApp.Models.Features.Loans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Interfaces
+{
+    public interface ILoanPresentationRepositoryProxy
+    {
+        Task<double> GetRepaymentProgressAsync(Loan loan);
+    }
+}
