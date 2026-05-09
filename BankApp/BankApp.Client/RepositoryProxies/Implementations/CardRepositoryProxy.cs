e1b2a9e1a599959fcc4669b38891c42fc3613d14
Kovács Levente <le.kovacs789@gmail.com>
2026-05-09 07:32:10 +0300

    created repo proxy for Cards API

---
+++ BankApp/BankApp.Client/RepositoryProxies/Implementations/CardRepositoryProxy.cs
@@ -0,0 +1,57 @@
+﻿using BankApp.Client.RepositoryProxies.Interfaces;
+using BankApp.Client.Utilities;
+using BankApp.Models.DTOs.Cards;
+using BankApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Implementations
+{
+    public class CardRepositoryProxy : ICardRepositoryProxy
+    {
+        private readonly ApiService apiService;
+
+        public CardRepositoryProxy(ApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public Task<List<Card>?> GetCardsAsync()
+        {
+            return this.apiService.GetAsync<List<Card>>("/api/cards");
+        }
+
+        public Task<Card?> GetCardAsync(int cardId)
+        {
+            return this.apiService.GetAsync<Card>($"/api/cards/{cardId}");
+        }
+
+        public Task<Account?> GetAccountAsync(int accountId)
+        {
+            return this.apiService.GetAsync<Account>($"/api/cards/account/{accountId}");
+        }
+
+        public Task<UserCardPreference?> GetSortPreferenceAsync()
+        {
+            return this.apiService.GetAsync<UserCardPreference>($"/api/cards/sortPreference");
+        }
+
+        public Task<bool> SaveSortPreferencesAsync(string sortOption)
+        {
+            return this.apiService.PutAsync<UpdateCardSortPreferenceRequest, bool>($"/api/cards/sortPreference/${sortOption}", new UpdateCardSortPreferenceRequest { });
+        }
+
+        public Task<bool> UpdateStatus(int cardId, string status)
+        {
+            return this.apiService.PutAsync<UpdateCardSettingsRequest, bool>($"/api/cards/{cardId}/updateStatus/{status}", new UpdateCardSettingsRequest { });
+        }
+
+        public Task<bool> UpdateStatus(int cardId, UpdateCardSettingsRequest request)
+        {
+            return this.apiService.PostAsync<UpdateCardSettingsRequest, bool>($"/api/cards/{cardId}/updateSettings", request);
+        }
+    }
+}
---
+++ BankApp/BankApp.Client/RepositoryProxies/Interfaces/ICardRepositoryProxy.cs
@@ -0,0 +1,18 @@
+﻿using BankApp.Models.DTOs.Cards;
+using BankApp.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Interfaces
+{
+    public interface ICardRepositoryProxy
+    {
+        Task<List<Card>?> GetCardsAsync();
+        Task<Card?> GetCardAsync(int cardId);
+        Task<Account?> GetAccountAsync(int accountId);
+        Task<UserCardPreference?> GetSortPreferenceAsync();
+        Task<bool> SaveSortPreferencesAsync(string sortOption);
+        Task<bool> UpdateStatus(int cardId, string status);
+        Task<bool> UpdateStatus(int cardId, UpdateCardSettingsRequest request);
+    }
+}
