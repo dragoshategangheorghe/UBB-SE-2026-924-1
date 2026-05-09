f60df13e5b3d2e42c2bc8714615ced6f8e059819
Kovács Levente <le.kovacs789@gmail.com>
2026-05-09 09:25:14 +0300

    created repo proxy for LoanDialogStateAPI

---
+++ BankApp/BankApp.Client/RepositoryProxies/Implementations/LoanDialogStateRepositoryProxy.cs
@@ -0,0 +1,25 @@
+﻿using BankApp.Client.RepositoryProxies.Interfaces;
+using BankApp.Client.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Implementations
+{
+    public class LoanDialogStateRepositoryProxy : ILoanDialogStateRepositoryProxy
+    {
+        private readonly ApiService apiService;
+
+        public LoanDialogStateRepositoryProxy(ApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public Task<bool> GetShouldComputeEstimateAsync(double desiredAmount, int preferredTermMonths, string purpose)
+        {
+            return this.apiService.GetAsync<bool>($"/api/loans/should-compute-estimate?desiredAmount={desiredAmount}&preferredTermMonths={preferredTermMonths}&purpose={purpose}");
+        }
+    }
+}
