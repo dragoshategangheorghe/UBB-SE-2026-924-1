a6dce1e0680d471675d19cbecca6dc7e2da8d821
Kovács Levente <le.kovacs789@gmail.com>
2026-05-09 08:12:34 +0300

    created repo proxy for Dashboard API

---
+++ BankApp/BankApp.Client/RepositoryProxies/Implementations/DashboardRepositoryProxy.cs
@@ -0,0 +1,41 @@
+﻿using BankApp.Client.RepositoryProxies.Interfaces;
+using BankApp.Client.Utilities;
+using BankApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Implementations
+{
+    public class DashboardRepositoryProxy : IDashboardRepositoryProxy
+    {
+        private readonly ApiService apiService;
+
+        public DashboardRepositoryProxy(ApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public Task<List<Card>?> GetCardsByUserAsync()
+        {
+            return this.apiService.GetAsync<List<Card>>($"/api/dashboard/cards");
+        }
+
+        public Task<List<Transaction>?> GetRecentTransactionsAsync()
+        {
+            return this.apiService.GetAsync<List<Transaction>>($"/api/dashboard/recentTransactions");
+        }
+
+        public Task<int> GetUnreadNotificationsCountAsync()
+        {
+            return this.apiService.GetAsync<int>($"/api/dashboard/unreadNotificationCount");
+        }
+
+        public Task<List<Account>?> GetAccountsByUserAsync()
+        {
+            return this.apiService.GetAsync<List<Account>>($"/api/dashboard/accounts");
+        }
+    }
+}
---
+++ BankApp/BankApp.Client/RepositoryProxies/Interfaces/IDashboardRepositoryProxy.cs
@@ -0,0 +1,14 @@
+﻿using BankApp.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Interfaces
+{
+    public interface IDashboardRepositoryProxy
+    {
+        Task<List<Card>?> GetCardsByUserAsync();
+        Task<List<Transaction>?> GetRecentTransactionsAsync();
+        Task<int> GetUnreadNotificationsCountAsync();
+        Task<List<Account>?> GetAccountsByUserAsync();
+    }
+}
