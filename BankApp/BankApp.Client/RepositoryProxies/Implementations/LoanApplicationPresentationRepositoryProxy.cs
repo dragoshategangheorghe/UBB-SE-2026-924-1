e12e5034cfec1a0a4e8d2d48c6c80b6f6e6f7e3d
Kovács Levente <le.kovacs789@gmail.com>
2026-05-09 09:17:05 +0300

    created proxy repo for LoanApplicationPresentationAPI

---
+++ BankApp/BankApp.Client/RepositoryProxies/Implementations/LoanApplicationPresentationRepositoryProxy.cs
@@ -0,0 +1,26 @@
+﻿using BankApp.Client.RepositoryProxies.Interfaces;
+using BankApp.Client.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Client.RepositoryProxies.Implementations
+{
+    public class LoanApplicationPresentationRepositoryProxy : ILoanApplicationPresentationRepositoryProxy
+    {
+        private readonly ApiService apiService;
+
+        public LoanApplicationPresentationRepositoryProxy(ApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public Task<(bool, string)> GetBuildApplicationOutcomeAsync(string? rejectionReason)
+        {
+            string reason = rejectionReason ?? string.Empty;
+            return this.apiService.GetAsync<(bool, string)>($"/api/loans/loan-application-presentation-outcome?rejectionReason={reason}");
+        }
+    }
+}
