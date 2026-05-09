c0bae528cffa2613a8adc5e0f40ad6613cf9e4b3
dragoshategangheorghe <dragos.hategan@stud.ubbcluj.ro>
2026-05-09 17:17:19 +0300

    Replaced insert files

--- DatabaseSchema/insert_account.sql
+++ DatabaseSchema/insert_account.sql
@@ -1,8 +1,8 @@
 use BankAppDb
 go
-select * from Users
-select * from Accounts
-select * from Cards
+select * from [User]
+select * from Account
+select * from Card
     DECLARE @FullName varchar(255)='test1'
     DECLARE @Now DATETIME2(0) = SYSUTCDATETIME();
     DECLARE @SeedStart DATETIME2(0) = CAST(DATEADD(DAY, -10, CAST(@Now AS DATE)) AS DATETIME2(0));
@@ -14,7 +14,7 @@ select * from Cards
     DECLARE @AccountIban VARCHAR(34) = CONCAT('RO49BAPP', RIGHT(REPLICATE('0', 22) + CAST(@UserId AS VARCHAR(22)), 22));
     DECLARE @CardNumber VARCHAR(19) = CONCAT('55554444', @UserSuffix);
     DECLARE @CardholderName NVARCHAR(200) = LEFT(COALESCE(NULLIF(@FullName, N''), N'Demo User'), 200);
-        INSERT INTO Accounts
+        INSERT INTO Account
         (
             UserId,
             AccountName,
