6f2e5ad3ef4adff7025dcef01046de8514f86881
Allex3 <103828759+Allex3@users.noreply.github.com>
2026-05-15 03:36:55 +0300

    Added Dependency Injection for services and proxy repositories in builder

--- BankApp/BankApp.Web/Program.cs
+++ BankApp/BankApp.Web/Program.cs
@@ -1,4 +1,6 @@
 using BankApp.Client.RepoProxies;
+using BankApp.Client.RepoProxies.Implementations;
+using BankApp.Client.RepoProxies.Interfaces;
 using BankApp.Client.Services.Implementations;
 using BankApp.Client.Services.Interfaces;
 
@@ -7,6 +9,43 @@ var builder = WebApplication.CreateBuilder(args);
 // Add controllers to the container.
 builder.Services.AddControllersWithViews();
 
+// ADD Client Proxy Repositories as singletons (because they don't actually store any data)
+// there are no dependencies below them, besides giving them the ApiService, which is truly independent
+
+builder.Services.AddSingleton<ApiService>();
+
+builder.Services.AddSingleton<IAccountRepoProxy, AccountRepoProxy>();
+builder.Services.AddSingleton<IAuthRepoProxy, AuthRepoProxy>();
+builder.Services.AddSingleton<ICardRepoProxy, CardRepoProxy>();
+builder.Services.AddSingleton<IChatRepoProxy, ChatRepoProxy>();
+builder.Services.AddSingleton<IDashboardRepoProxy, DashboardRepoProxy>();
+builder.Services.AddSingleton<IInvestmentsRepoProxy, InvestmentsRepoProxy>();
+builder.Services.AddSingleton<ILoanApplicationPresentationRepoProxy, LoanApplicationPresentationRepoProxy>();
+builder.Services.AddSingleton<ILoanDialogStateRepoProxy, LoanDialogStateRepoProxy>();
+builder.Services.AddSingleton<ILoansRepoProxy, LoansRepoProxy>();
+builder.Services.AddSingleton<IProfileRepoProxy, ProfileRepoProxy>();
+builder.Services.AddSingleton<ISavingsPresentationRepoProxy, SavingsPresentationRepoProxy>();
+builder.Services.AddSingleton<ISavingsRepoProxy, SavingsRepoProxy>();
+builder.Services.AddSingleton<ISavingsUiRulesRepoProxy, SavingsUiRulesRepoProxy>();
+builder.Services.AddSingleton<ISavingsWorkflowRepoProxy, SavingsWorkflowRepoProxy>();
+builder.Services.AddSingleton<IStatisticsRepoProxy,  StatisticsRepoProxy>();
+builder.Services.AddSingleton<ITransactionRepoProxy, TransactionRepoProxy>();
+
+
+// ADD CLIENT SERVICES AS SINGLETONS
+
+builder.Services.AddSingleton<IAccountService, AccountService>();
+builder.Services.AddSingleton<IAuthService, AuthService>();
+builder.Services.AddSingleton<ICardService, CardService>();
+builder.Services.AddSingleton<IChatService, ChatService>();
+builder.Services.AddSingleton<IDashboardService, DashboardService>();
+builder.Services.AddSingleton<IInvestmentsService, InvestmentsService>();
+builder.Services.AddSingleton<ILoansService, LoansService>();
+builder.Services.AddSingleton<INotificationClientService, NotificationClientService>();
+builder.Services.AddSingleton<IProfileService, ProfileService>();
+builder.Services.AddSingleton<ISavingsService, SavingsService>();
+builder.Services.AddSingleton<IStatisticsService, StatisticsService>();
+builder.Services.AddSingleton<ITransactionHistoryService, TransactionHistoryService>();
 
 var app = builder.Build();
 
